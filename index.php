260b3f1eb320494b1fa3a5482b9cc1c270a76525
frdlnaji <106515917+frdlnaji@users.noreply.github.com>
2023-05-12 00:34:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BujangGanteng.com</title>
+</head>
+
+<body style="background:darkgray;">
+    <div style="color:rgb(252, 252, 252); padding: 30px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+        <h1 style="color: blue;">Selamat Datang di Demo APP Engine</h1>
+        <h3>Ini adalah Halaman Beranda</h3>
+    </div>
+</body>
+
+</html>
